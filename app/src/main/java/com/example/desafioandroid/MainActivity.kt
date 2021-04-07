67001a3740995ce51afe5cb4c73aa6eaf2d520fc
Adilson Junior <asc.juninho@hotmail.com>
2021-04-07 17:30:26 -0300

    Adiciona implementação da activity Main

--- app/src/main/java/com/example/desafioandroid/MainActivity.kt
+++ app/src/main/java/com/example/desafioandroid/MainActivity.kt
@@ -15,9 +15,42 @@ import com.example.desafioandroid.model.Cor
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+  val LIST_VIEW = 1
+
+  private lateinit var cadastro: Cadastro
+  private lateinit var listView: ListView
+  private lateinit var floatingActionButton: FloatingActionButton
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    this.cadastro = Cadastro()
+
+    this.listView = findViewById(R.id.mainLvCor)
+    this.listView.adapter = CorAdapter(cadastro, this)
+
+    this.floatingActionButton = findViewById(R.id.mainFabAdd)
+    this.floatingActionButton.setOnClickListener(ClickFloatingActionButton())
+  }
+
+  inner class ClickFloatingActionButton: View.OnClickListener {
+    override fun onClick(v: View?) {
+      val intent = Intent(this@MainActivity, FormActivity::class.java)
+      startActivityForResult(intent, LIST_VIEW)
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (resultCode == RESULT_OK) {
+      if (requestCode == LIST_VIEW) {
+        val cor = data?.getSerializableExtra("COR") as Cor
+
+        (this.listView.adapter as CorAdapter).add(cor)
+        (this.listView.adapter as CorAdapter).notifyDataSetChanged()
+      }
+    }
   }
 }
